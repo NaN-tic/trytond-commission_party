82c31e89ee37f6b4209664a178a611da9bd38ae6
Carlos G?lvez <carlos.galvez@nan-tic.com>
2019-06-06 09:40:17 +0200

    Link the commissioner to the recipient, not to the party. #037160

--- tests/test_commission_party.py
+++ tests/test_commission_party.py
@@ -1,16 +1,17 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import suite as test_suite
 
 
-class TestCase(ModuleTestCase):
-    'Test module'
+class CommissionPartyTestCase(ModuleTestCase):
+    'Test Commission Party module'
     module = 'commission_party'
 
 
 def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite = test_suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+            CommissionPartyTestCase))
     return suite
